9d527fd87ccda5587a3a580ec0753b53bf952ca5
SudoWatson <aulennert@gmail.com>
2021-08-30 01:49:13 -0500

    Idk man hopefully not nothing

--- public/javascript/runnable.js
+++ public/javascript/runnable.js
@@ -7,35 +7,30 @@
   let key = e.which;
   /** Div containing tag relevent elements */
   let div = document.getElementsByClassName("tagField")[0]
-  /** Textarea field where user enters the tag to add */
-  let tagField = document.getElementById("tag-input")
+  /** Textarea field where user enters the new tag to add */
+  let newTag = document.getElementById("new-tag")
   /** Invisible input field where tags are moved to. Value of this is actually submitted for the tags */
-  let realTagField = document.getElementById("tags")
+  let tagField = document.getElementById("tags")
 
-  console.log("Hello")
   if (key == 32) {  // If 'space' key is pressed, add tag
-      if (tagField.value.trim() === "") return  // Don't add empty field
+      if (newTag.value.trim() === "") return  // Don't add empty field
 
       /** Entered tag */
-      let tag = tagField.value
-      tagField.value = " "
-      console.log(realTagField.value)
-      console.log(tag.trim() + " ")
-      if (realTagField.value.includes((tag.trim() + " "))) return  // Tag already entered
+      let tag = newTag.value
+      newTag.value = " "
+      if (tagField.value.includes((tag.trim() + " "))) return  // Tag already entered
       
       /** Span element containing new tag */
       let span = document.createElement("span")
-      div.insertBefore(span, tagField)
+      div.insertBefore(span, newTag)
       span.appendChild(document.createTextNode(tag.trim()))
-      realTagField.value = realTagField.value + tag.trim() + " "  // Don't add unnecessary whitespace
-      tagField.cols = tagField.cols - tag.length
-      
-      //tagField.style.width = getComputedStyle(tagField).width - getComputedStyle(span).width;
-  } else if (key === 8 && tagField.value === "" && div.getElementsByTagName("span").length > 0) {  // 'Backspace' - Remove last tag 
+      tagField.value = tagField.value + tag.trim() + " "  // Add tag to full list
+      tagField.style.width = getComputedStyle(tagField).width - getComputedStyle(span).width;
+  } else if (key === 8 && newTag.value === "" && div.getElementsByTagName("span").length > 0) {  // 'Backspace' - Remove last tag 
       let lastTag = div.getElementsByTagName("span")
       lastTag = lastTag[lastTag.length-1]
-      realTagField.value = realTagField.value.slice(0, -(lastTag.textContent.length + 1))
-      tagField.value = " " + lastTag.textContent
+      tagField.value = tagField.value.slice(0, -(lastTag.textContent.length + 1))
+      newTag.value = " " + lastTag.textContent
       lastTag.parentNode.removeChild(lastTag)
   }
 }
