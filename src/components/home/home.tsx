c0915f267f3b00ee8a89375d909cc642ff9c9f5f
SudoWatson <aulennert@gmail.com>
2022-12-11 20:57:08 -0600

    Home Background Image

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -18,6 +18,7 @@ export default class home extends Component<Props, State> {
             < Links />
             <a href="#About"><span className="material-symbols-outlined expand_more">expand_more</span></a>
         </div>
+        <img src="/imgs/stars.jpg" id="HeaderImage" />
       </div>
     )
   }
