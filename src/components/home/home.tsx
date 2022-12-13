f8bfe6ee4fc668518d787f6e956728bb8fe233f7
SudoWatson <aulennert@gmail.com>
2022-12-12 20:52:10 -0600

    Fixed Arrow not Positioning Correctly

--- src/components/home/home.tsx
+++ src/components/home/home.tsx
@@ -16,9 +16,10 @@ export default class home extends Component<Props, State> {
         <div className='center'>
             <h1>Hi, I'm Austin Lennert</h1>
             < Links />
-            <a href="#About"><span className="material-symbols-outlined expand_more">expand_more</span></a>
+            <a id="scrollDownArrow" href="#About"><span className="material-symbols-outlined expand_more">expand_more</span></a>
         </div>
         <img src="/imgs/stars.jpg" id="HeaderImage" />  // TODO Change this to something cooler and more programmy
+        // TODO Not sizing well on other screen
       </div>
     )
   }
