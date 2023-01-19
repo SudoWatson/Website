efe1a1498bf651c4a40dc9a4d5523847e9962414
SudoWatson <aulennert@gmail.com>
2023-01-18 18:29:43 -0600

    Basic setup for responsiveness

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -17,7 +17,6 @@ import Footer from "./components/footer/footer";
 export default function App() {
 	return (
 		<div>
-			<div id="mobileWarning"><span>WARNING!</span> Please note that this is the first version of this website, and also the first website I have ever built. At this current time the site is not altered to display nicely on mobile devices. While some larger screen mobile devices may view the contents okay, it is very likely that the page will look bad and broken on many others. This is something I WILL fix, but in this first version it is best to view in desktop mode, or better yet, on a computer screen. Thank you</div>
 			<Home />
 			<Header />
 			<div className="container-fluid">
