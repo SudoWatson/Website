628a71d0c6a4b51b42b3eb5efd5a28f61291483b
SudoWatson <aulennert@gmail.com>
2024-01-26 20:56:14 -0600

    Update information post graduation

--- client/src/components/about/about.tsx
+++ client/src/components/about/about.tsx
@@ -17,10 +17,10 @@ export default class about extends Component<Props, State> {
         {/* eslint-disable-next-line*/}
         <p className="comment">// About</p>
         <div id="aboutContent">
-        <p id="aboutMe">Howdy! I'm Austin Lennert, currently a first-year student at Fox Valley Technical College pursuing an Associate's Degree in Software Development
-          and a Technical Diploma in Web Design. For 4+ years prior to attending college I self-taught myself the core concepts of programming, as well as a few languages
-          on the way. I am continuously exposing myself to new technologies, am ever curious to expand my knowledge in every facet of software, and am always eager
-          to learn something new!
+        <p id="aboutMe">Howdy! I'm Austin Lennert, a recent graduate of Fox Valley Technical College and a software engineer at Skyward helping develop administration 
+          software for K-12 school districts. I am focused in development using C#, Entity Framework Core, SQL, and Blazor in my work, personal projects, and from school.
+          I spent 4 years before school teaching myself programming, focusing then on Java, TypeScript, and just getting a sound understanding of software development. I am
+          continuously exposing myself to new technologies, am ever curious  to expand my knowledge in every facet of software, and am always eager to learn something new!
         </p>
         {/* <img id="bbimg" src={BBCPU_IMG_PATH} alt="The breadboard computer I built, more on this coming soon" /> */}
         </div>
