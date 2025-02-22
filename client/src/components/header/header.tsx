cf16a4d29397ac0f0acdf70c6c080d7b81d25341
SudoWatson <aulennert@gmail.com>
2025-02-21 20:53:31 -0500

    Add logo and fail add icon

--- client/src/components/header/header.tsx
+++ client/src/components/header/header.tsx
@@ -14,14 +14,19 @@ export default class header extends Component<Props, State> {
     render() {
         return (
             <nav id='main-nav'>
-                <ul id='inpage-links'>
-                    <li><a href='#Home'>Home</a></li>
-                    <li><a href='#About'>About</a></li>
-                    <li><a href='#Skills'>Skills</a></li>
-                    <li><a href='#Projects'>Projects</a></li>
-                    <li><a href='#Contact'>Contact</a></li>
-                </ul>
-                <Links />
+                <div id='left'>
+                    <ul id='inpage-links'>
+                        <li><a href='#Home'>Home</a></li>
+                        <li><a href='#About'>About</a></li>
+                        <li><a href='#Skills'>Skills</a></li>
+                        <li><a href='#Projects'>Projects</a></li>
+                        <li><a href='#Contact'>Contact</a></li>
+                    </ul>
+                </div>
+                <img id="logo" width={"10%"} style={{ filter: "invert(0.85)" }} src='imgs/logos/AustinLogo-01.png' />
+                <div id='right'>
+                    <Links />
+                </div>
             </nav>
         )
     }
--- server/app.ts
+++ server/app.ts
@@ -1,12 +1,14 @@
 import express, { Request, Response, NextFunction } from 'express';
+import favicon from 'serve-favicon';
 
 // Load .env
 import dotenv from 'dotenv';
 import path from 'path';
-dotenv.config({path: '../.env'});
+dotenv.config({ path: '../.env' });
 
 // Create Server
 const app = express();
+
 const port = process.env.PORT;
 
 // ---===  USES  ===--- \\
@@ -15,25 +17,27 @@ const port = process.env.PORT;
 // ---===  END   ===--- \\
 
 
+let publicImagePath: string;
 // ---=== ROUTES ===--- \\
 
 if (process.env.NODE_ENV === "development") {
   console.log("Running in DEVELOPMENT mode");
-  
+  publicImagePath = path.join(__dirname, '..', 'client', 'public', 'imgs', 'logos', 'AustinIcon-02.png');
+
   // If in development, proxy '/' to the hosted React server
   let proxy = require("http-proxy-middleware");
 
   app.use('/', proxy.createProxyMiddleware({
-    target:"http://127.0.0.1:3000",
+    target: "http://127.0.0.1:3000",
     changeOrigin: true
   }));
-  
 } else {
   console.log("Running in PRODUCTION mode");
   // If in production, serve build path
   let reactPath = path.join("..", process.env.CLIENT_BUILD_PATH);
   app.use('/', express.static(reactPath));
 }
+app.use(favicon(publicImagePath));
 
 app.post('/email', (req: Request, res: Response) => {
   // TODO Handle email
